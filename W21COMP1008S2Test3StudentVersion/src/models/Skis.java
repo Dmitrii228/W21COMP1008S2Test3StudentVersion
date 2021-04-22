f3ba1d4c3b11699955bc0686147fd0b737234190
Dmitrii228 <200464709@student.georgianc.on.ca>
2021-04-22 13:58:03 -0400

    question 6 complete

--- W21COMP1008S2Test3StudentVersion/src/models/Skis.java
+++ W21COMP1008S2Test3StudentVersion/src/models/Skis.java
@@ -6,11 +6,18 @@ package models;
 
 import Utilities.DBUtility;
 
-public class Skis {
+public class Skis extends InventoryItem{
     private String brand, model;
     private int length;
 
     public Skis(String brand, String model, int length,double purchasePrice, double sellingPrice, int quantityInStock) {
+        super(purchasePrice, sellingPrice, quantityInStock);
+        setBrand(brand);
+        setModel(model);
+        setLength(length);
+        setPurchasePrice(purchasePrice);
+        setSellingPrice(sellingPrice);
+        setQuantityInStock(quantityInStock);
     }
 
     public String getBrand() {
@@ -32,5 +39,10 @@ public class Skis {
     }
 
     public void setLength(int length) {
+        if (length >= 60 && length <= 220){
+            this.length = length;
+        }
+        else
+            throw new IllegalArgumentException("The length is invalid, should be in the range between 60 and 220!");
     }
 }
